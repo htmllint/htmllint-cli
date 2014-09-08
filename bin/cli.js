d0799ca3fd1169d8607b00a06b22bfa8a2ac6036
Coalman <tehcoalman@gmail.com>
2014-09-08 13:26:40 -0400

    added basic htmllint cli implementation

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,28 @@
 #!/bin/env node
 
-var htmllint = require('htmllint');
+var Liftoff = require('liftoff'),
+    argv = require('minimist')(process.argv.slice(2)),
+    fs = require('fs');
+
+var app = new Liftoff({
+    processTitle: 'htmllint',
+    moduleName: 'htmllint',
+    configName: 'htmllintrc'
+});
+
+app.launch({
+    cwd: argv.cwd,
+    configPath: argv.rc
+}, function (env) {
+    var htmllint = require(env.modulePath),
+        rc = env.configPath ? require(env.configPath) : {},
+        filesrc = fs.readFileSync(argv['_'][0]);
+
+    var issues = htmllint(filesrc, rc);
+
+    issues.forEach(printIssue);
+});
+
+function printIssue(issue) {
+    console.log('(' + issue.line + ', ' + issue.column + '): ' + issue.msg);
+}
